acb556a962afb2d3a8cdd53ad09b21c7a2a86bef
Zaki <33980130+benaissazaki@users.noreply.github.com>
2022-09-29 23:32:57 +0100

    Move sfml loop to main (#3)
    * Make width and height static
    
    * Move sfml loop to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "Renderer.h"
 #include "Keyboard.h"
 
-int main() {
-    Renderer renderer(10);
+int main()
+{
+    float scale = 10;
+    int windowWidth = Renderer::width * scale;
+    int windowHeight = Renderer::height * scale;
+
+    Renderer renderer(scale);
     Keyboard keyboard;
+
     renderer.setPixel(20, 20);
     renderer.setPixel(40, 15);
-    renderer.render();
+
+    // Create window
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Chip 8 Emulator");
+    window.setFramerateLimit(60);
+
+    // Create toggled pixel
+    sf::RectangleShape pixel(sf::Vector2f(scale, scale));
+    pixel.setFillColor(sf::Color::White);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        renderer.render(&window, pixel);
+        window.display();
+    }
+
     return 0;
 }
